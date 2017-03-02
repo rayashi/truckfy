93c007f12770c6e57c7ad5e07d04d70bb16500ea
Humberto Dib <humbertodib@cocaudi.com.br>
2017-03-02 16:11:39 -0300

    Ajuste truck media path

--- apps/personas/utils.py
+++ apps/personas/utils.py
@@ -4,9 +4,8 @@ from uuid import uuid4
 
 def truck_path(instance, filename):
     ext = filename.split('.')[-1]
-    filename = '{}.{}'.format(uuid4().hex, ext)
-    path = os.path.join("trucks", str(instance.id))
-    return os.path.join(path, filename)
+    filename = '{}-{}.{}'.format(instance.id, uuid4().hex, ext)
+    return os.path.join("trucks", filename)
 
 
 def client_path(instance, filename):
