d2cce8b1e7d04c8285b62d6add09486ffe05d035
Humberto Dib <humbertodib@cocaudi.com.br>
2017-03-22 17:13:06 -0300

    Follows End points ajusts

--- apps/reviews/signals.py
+++ apps/reviews/signals.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db.models.signals import post_delete, post_save, pre_delete
 from django.dispatch import receiver
+from django.db.models import Avg
 
 from apps.reviews.models import *
 
@@ -8,6 +9,7 @@ from apps.reviews.models import *
 @receiver(pre_delete, sender=Review)
 def client_deleted(sender, instance, **kwargs):
     instance.truck.review_amount -= 1
+    instance.truck.review_rate = instance.truck.review_set.all().aggregate(Avg('rate')).get('rate__avg')
     instance.truck.save()
 
 
